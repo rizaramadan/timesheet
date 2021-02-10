b14af6136d5cde621a6a72ad55408a9b85d3374a
Riza Ramadan <riza.ramadan@gmail.com>
2021-02-10 23:02:41 +0700

    Add project files.

---
+++ Timesheet/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timesheet.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
